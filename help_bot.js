e14757cda84f8f2d428375ad4a04cc855c49103d
Roman <100799424+ROMAZAST@users.noreply.github.com>
2025-03-13 18:25:12 +0200

    Adding comments  and some fixes in code

--- help_bot.js
+++ help_bot.js
@@ -1,24 +1,25 @@
 
 const {admins_id, bot_help} = require("./const.js")
 const {  updateAnswerByNumber, setAnswerRestorByNumber,setAnswerDeleteByNumber, open_ask, open_menu, setAnswerDelete,openDocument, open_tickets, getDocumentById,generateQuestionButtons,add_question_to_db, main_menu, findTicketsByIdAndAnswer} = require("./functions_for_help_bot.js")
-
+// starting the help bot
 bot_help.onText(/^\/start/, (msg) => {
   main_menu(msg)
 });
+// main menu 
 bot_help.onText(/^\/menu/, (msg) => {
   main_menu(msg)
 });
-
+// handling the /ask command for adding the question to the database
 bot_help.onText(/^\/ask(.*)/, (msg,match) => {
   add_question_to_db(msg,match)
 });  
-
+// handling the /lastask command for showing the tickets to the admins
 bot_help.onText(/^\/lastask/, (msg) => {
   var message = msg.chat.id
   if (admins_id.indexOf(String(message)) !== -1){ open_ask(msg) }
   else { /*bot_help.sendMessage(msg.chat.id, `У вас немає доступу до цієї команди`)*/ }
 });  
-
+// handling the /delete command for deleting the question by number
 bot_help.onText(/^\/delask(.*)/, (msg,match) => {
   var message = msg.chat.id
   if (admins_id.indexOf(String(message)) !== -1) {
@@ -27,7 +28,7 @@ bot_help.onText(/^\/delask(.*)/, (msg,match) => {
   }
   else { /*bot_help.sendMessage(msg.chat.id, `У вас немає доступу до цієї команди)`)*/ }
 }); 
-
+// handling the /resask command for restoring the question by number
 bot_help.onText(/^\/resask(.*)/, (msg,match) => {
   var message = msg.chat.id
   if (admins_id.indexOf(String(message)) !== -1){
@@ -36,10 +37,11 @@ bot_help.onText(/^\/resask(.*)/, (msg,match) => {
   }
   else { /*bot_help.sendMessage(msg.chat.id, `У вас немає доступу до цієї команди)`)*/ }
 });  
+// handling the /id command for showing the id of the user
 bot_help.onText(/^\/id/, (msg) => {
   bot_help.sendMessage(msg.chat.id, `Ваш id: ${msg.chat.id}`)
 });  
-
+// handling the /answ command for updating the answer by number for admins
 bot_help.onText(/^\/answ \b(.*)\b (.*)/, (msg, match) => {
   updateAnswerByNumber(match[1], match[2])
 });
@@ -48,22 +50,22 @@ bot_help.on('callback_query', (callbackQuery) => {
   const message = callbackQuery.message;
   const data1 = callbackQuery.data;
 
-  if (callbackQuery.data === 'tickets') { open_tickets(callbackQuery, 1) }
-  if (callbackQuery.data === `menu`) { open_menu(callbackQuery) }
-  else if (callbackQuery.data === 'back_to_tickets') { open_tickets(callbackQuery, 1) }
-  else if (data1.startsWith('open_question:')) {
+  if (callbackQuery.data === 'tickets') { open_tickets(callbackQuery, 1) } // open the tickets
+  if (callbackQuery.data === `menu`) { open_menu(callbackQuery) } // open the main menu
+  else if (callbackQuery.data === 'back_to_tickets') { open_tickets(callbackQuery, 1) }// back to the tickets
+  else if (data1.startsWith('open_question:')) { // open the question
     const id_list = data1.split(':')[1];
     getDocumentById(id_list).then(doc => { openDocument(callbackQuery,doc) })
   }
-  else if (data1.startsWith('next_page:')) {
+  else if (data1.startsWith('next_page:')) {// next page tickets
     const page = data1.split(':')[1];
     open_tickets(callbackQuery, page)
   }
-  else if (data1.startsWith('previous_page:')) {
+  else if (data1.startsWith('previous_page:')) {// previous page tickets
     const page = data1.split(':')[1];
     open_tickets(callbackQuery, page)
   }
-  else if (data1.startsWith('delete_ticket:')) {
+  else if (data1.startsWith('delete_ticket:')) {// delete the ticket by number
     const number = data1.split(':')[1];
     setAnswerDeleteByNumber(number).then( setTimeout(open_tickets,1000,callbackQuery, 1))
   }
--- main_bot.js
+++ main_bot.js
@@ -9,6 +9,7 @@ password,login} = require("./functions_for_main_bot.js")
 const {admins_id,links,emodji, client, bot, responce_answer} = require("./const.js")
 const start_help_bot = require('./help_bot.js');
 client.connect();
+// starting the bot
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const photoUrl = links.auth;
@@ -18,14 +19,16 @@ bot.onText(/\/start/, (msg) => {
   const options = { caption: text };
   bot.sendPhoto(chatId, photoUrl, options);
 });
+// handling the /name command - changing the name of the list
 bot.onText(/^\/name(.*)/, (msg, match) => {
   const chatId = msg.chat.id;
   name_change_update(msg, msg.chat.id, match[1])
 });
+// handling the /l(.*) command - login and logout  commands
 bot.onText(/^\/l(.*)/, (msg, match) => {
   login(msg.chat.id,match)
 });
-
+// handling the /menu command - opening the main menu
 bot.onText(/^\/menu/, (msg) => {
   const photoUrl = links.main_menu;
   const keyboard = {
@@ -37,19 +40,18 @@ bot.onText(/^\/menu/, (msg) => {
   };
   bot.sendPhoto(msg.chat.id, photoUrl, { reply_markup: keyboard });
 });
-
+// handling the /pass command - registration in the system
 bot.onText(/^\/pass(.*)/, (msg, match) => {
   const chatId = msg.chat.id;
   password(chatId,match);
 });
-
 bot.on('polling_error', (err) => console.log(err));
-
+// handling the all another command
 bot.on('callback_query', (callbackQuery) => {
   const message = callbackQuery.message;
   const data1 = callbackQuery.data;
   const chatId = callbackQuery.from.id;
-  
+  // handling the callback data - help menu
   if (callbackQuery.data === 'help') {
     const message = callbackQuery.message;
     const targetBotLink = 'https://t.me/help_listers_bot';
@@ -74,98 +76,96 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); }); 
     bot.answerCallbackQuery(callbackQuery.id);
-  }
+  }// handling the callback data - main menu
   else if (callbackQuery.data === 'menu') {
     getUser(chatId).then(user => {
       if(user.auth !== true){ say(chatId, 0); }
       else if(user.auth == true){ menu(callbackQuery); }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - settings menu
   else if (callbackQuery.data === 'settings') {
     getUser(chatId).then(user => {
       if(user.auth !== true){ say(chatId, 0); }
       else if(user.auth == true){ settings(callbackQuery); }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - reminder menu
   else if (callbackQuery.data === 'reminder') {
     getUser(chatId).then(user => {
       if(user.auth !== true){ say(chatId, 0) }
       else if(user.auth == true){ reminder(callbackQuery); }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for off reminder
   else if (callbackQuery.data === 'off_reminder') {
     getUser(chatId).then(user => {
       if(user.auth !== true){ say(chatId, 0) }
       else if(user.auth == true){ off_reminder(callbackQuery); }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for on reminder
   else if (callbackQuery.data === 'on_reminder') {
     getUser(chatId).then(user => {
       if(user.auth !== true){ say(chatId, 0) }
       else if(user.auth == true){ on_reminder(callbackQuery); }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for logout
   else if (callbackQuery.data === 'logout') {
     logOut(message.chat.id);
     bot.answerCallbackQuery(callbackQuery.id);
-  }
+  }// handling the callback data - command for open lists
   else if (callbackQuery.data === 'lists') {
     getUser(chatId).then(user => {
       if(user.auth !== true){ say(chatId, 0); }
       else if(user.auth == true) { lists(callbackQuery) }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  } 
+  } // handling the callback data - command for open shared lists
   else if (callbackQuery.data === 'open_shared_lists') {
     getUser(chatId).then(user => {
       if(user.auth !== true){ say(chatId, 0); }
       else if(user.auth == true) { open_shared_lists(callbackQuery) }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  } 
-  
+  } // handling the callback data - command add list
   else if (callbackQuery.data === 'list_add') {
     getUser(chatId).then(user => {
       if(user.auth !== true){ say(chatId, 0); }
       else if(user.auth == true){ addList(callbackQuery) }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for open profile menu
   else if (callbackQuery.data === 'profile') {
     getUser(chatId).then(user => {
       if(user.auth !== true) { say(chatId, 0); }
       else if(user.auth == true) { profile(callbackQuery) }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
-  
+  }// handling the callback data - command for open list names
   else if (callbackQuery.data === 'lists_out') {
     getUser(chatId).then(user => {
       if(user.auth !== true) { say(chatId, 0); }
       else if(user.auth == true) { lists_out(callbackQuery, ``, 1) }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for show message about the count of product which will be added
   else if (callbackQuery.data === 'count_to_add') {
     bot.answerCallbackQuery(callbackQuery.id, { text: '~§ Це кількість продукту, яку буде додано до Вашого списку §~' });
-  }
+  }// handling the callback data - command for show message about the name of the list
   else if (callbackQuery.data === 'list_name') {
     bot.answerCallbackQuery(callbackQuery.id, { text: '~§ Це назва Вашого списку §~' });
-  }
+  }// handling the callback data - command for show message about the total price of the list
   else if (callbackQuery.data === 'list_total_price') {
     bot.answerCallbackQuery(callbackQuery.id, { text: '~§ Це загальна ціна Вашого списку §~' });
-  }
+  }// handling the callback data - command for show message about the name of the list
   else if (callbackQuery.data === 'list_name1') {
     bot.answerCallbackQuery(callbackQuery.id, { text: '~§ Це назва списку §~' });
-  }
+  }// handling the callback data - command for show message about the total price of the list
   else if (callbackQuery.data === 'list_total_price1') {
     bot.answerCallbackQuery(callbackQuery.id, { text: '~§ Це загальна ціна списку §~' });
-  }
+  }// handling the callback data - command for open list by name
   else if (data1.startsWith('openlist:')) {
     const id_list = data1.split(':')[1];
     getUser(chatId).then(user => {
@@ -173,7 +173,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true) { openList(id_list).then(list => { openListbykeys(callbackQuery, list) }); }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  } // handling the callback data - command for delete list
   else if (data1.startsWith('delete_list:')) {
     const id_list = data1.split(':')[1];
     getUser(chatId).then(user => {
@@ -181,7 +181,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true) { delete_list(id_list).then(result => { lists_out(callbackQuery, `Список видалено`, 1) }); }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for delete list by admin
   else if (data1.startsWith('delete_list_adm:')) {
     const id_list = data1.split(':')[1];
     getUser(chatId).then(user => {
@@ -189,7 +189,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true) { delete_list(id_list).then(result => { open_shared_lists(callbackQuery) }); }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for edit list
   else if (data1.startsWith('edit_list:')) {
     const _id = data1.split(':')[1];
     getUser(chatId).then(user => {
@@ -197,7 +197,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true) { edit_list(callbackQuery, _id) }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for show next page of the product list
   else if (data1.startsWith('next_page:')) {
     const page = data1.split(':')[1];
     const _id = data1.split(':')[2];
@@ -206,7 +206,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true) { add_product(callbackQuery, _id, page) }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for show previous page of the product list
   else if (data1.startsWith('previous_page:')) {
     const page = data1.split(':')[1];
     const _id = data1.split(':')[2];
@@ -215,16 +215,15 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true) { add_product(callbackQuery, _id, page) }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for show next page of the list
   else if (data1.startsWith('nextlists_page:')) {
     const page = data1.split(':')[1];
-    //const _id = data1.split(':')[2];
     getUser(chatId).then(user => {
       if(user.auth !== true) { say(chatId, 0); }
       else if(user.auth == true) { lists_out(callbackQuery, ``, page) }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for show previous page of the list
   else if (data1.startsWith('prevlists_page:')) {
     const page = data1.split(':')[1];
     getUser(chatId).then(user => {
@@ -232,7 +231,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true) { lists_out(callbackQuery, ``, page) }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for change name of the list
   else if (data1.startsWith('change_name:')) {
     const message = callbackQuery.message;
     const _id = data1.split(':')[1];
@@ -251,7 +250,7 @@ bot.on('callback_query', (callbackQuery) => {
       }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-   }
+  }// handling the callback data - command for add product to the list
   else if (data1.startsWith('add_product:')) {
     const _id = data1.split(':')[1];
     const page = data1.split(':')[2];
@@ -260,25 +259,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true) { add_product(callbackQuery, _id, page) }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }  
-  else if (data1.startsWith('previous_page_:')) {
-    const page = data1.split(':')[1];
-    const _id = data1.split(':')[2];
-    getUser(chatId).then(user => {
-      if(user.auth !== true){ say(chatId, 0) }
-      else if(user.auth == true){ add_product(callbackQuery, _id, page); }
-      else if(user.auth == null){setAuthUser(chatId, false); say(chatId, 1);}
-    }).catch(err => { console.error(err); });
-  }
-  else if (data1.startsWith('add_product_:')) {
-    const _id = data1.split(':')[1];
-    const page = data1.split(':')[2];
-    getUser(chatId).then(user => {
-      if(user.auth !== true){ say(chatId, 0); }
-      else if(user.auth == true){ add_product(callbackQuery, _id, page) }
-      else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
-    }).catch(err => { console.error(err); });
-  }  
+  }  // handling the callback data - command for open section
   else if (data1.startsWith('open_section:')) {
     const name_sect = data1.split(':')[1];
     const id_sect = data1.split(':')[2];
@@ -289,7 +270,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true){ open_section(callbackQuery, name_sect, _id, id_sect,page) }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for open confirm menu to the share list by user
   else if (data1.startsWith('share_list:')) {
     const _id = data1.split(':')[1];
     getUser(chatId).then(user => {
@@ -297,7 +278,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true) { share_list_confirm(callbackQuery, _id) }
       else if(user.auth == null) { setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  } 
+  } // handling the callback data - command for show message about the confirm share list
   else if (data1.startsWith('share_list_true:')) {
     const _id = data1.split(':')[1];
     getUser(chatId).then(user => {
@@ -309,7 +290,7 @@ bot.on('callback_query', (callbackQuery) => {
       }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  } 
+  } // handling the callback data - command for show message about the decline share list
   else if (data1.startsWith('share_list_false:')) {
     const _id = data1.split(':')[1];
     getUser(chatId).then(user => {
@@ -321,7 +302,7 @@ bot.on('callback_query', (callbackQuery) => {
       }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  } 
+  } // handling the callback data - command for show new count of product for add to the list
   else if (data1.startsWith('count_new:')) {
     const name = data1.split(':')[1];
     const _id = data1.split(':')[2];
@@ -334,7 +315,7 @@ bot.on('callback_query', (callbackQuery) => {
         else if(count == 0){ bot.answerCallbackQuery(callbackQuery.id,{ text: `Додати до списку можна не менше, ніж 1 продукт`}); }}
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for add product to the list
   else if (data1.startsWith('add_to_list:')) {
     const _id = data1.split(':')[1];
     const count = data1.split(':')[2];
@@ -344,7 +325,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true){ add_element_to_list(callbackQuery,name_prod,count,_id) }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1);}
     }).catch(err => { console.error(err); });
-  }
+  }// handling the callback data - command for confirm add product to the list
   else if(data1.startsWith('add_conf:')) {
     const id_sect = data1.split(':')[1];//  id section
     const name = data1.split(':')[2]; // id product
@@ -355,7 +336,7 @@ bot.on('callback_query', (callbackQuery) => {
       else if(user.auth == true){ confirm_menu(callbackQuery,name, _id, count, id_sect) }
       else if(user.auth == null){ setAuthUser(chatId, false); say(chatId, 1); }
     }).catch(err => { console.error(err); })
-  }  
+  }// handling the callback data - command for open shared list by user
   else if(data1.startsWith('open_shared_list:')) {
     const _id = data1.split(':')[1];
     var id = callbackQuery.from.id
